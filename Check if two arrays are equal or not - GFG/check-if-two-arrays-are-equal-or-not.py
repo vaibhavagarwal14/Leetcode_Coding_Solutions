66d3cb72e31f7428b3ef2cae23e98f1f5eb8d75d
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-06-21 01:35:51 +0530

    Added solution - LeetHub

---
+++ Check if two arrays are equal or not - GFG/check-if-two-arrays-are-equal-or-not.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    #Function to check if two arrays are equal or not.
+    def check(self,A,B,N):
+        
+        #return: True or False
+        
+        #code here
+        count={}
+        for i in A:
+            if i in count:
+                count[i]+=1
+            else:
+                count[i]=1
+        for i in B:
+            if i not in count or count[i]==0:
+                return False
+            else:
+                count[i]-=1
+        return True
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for tc in range(t):
+        
+        N=int(input())
+        
+        A = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        B = [int(x) for x in input().replace('  ',' ').strip().split(' ')]
+        ob=Solution()
+        if ob.check(A,B,N):
+            print(1)
+        else:
+            print(0)
+        
+                
+                
+# } Driver Code Ends
