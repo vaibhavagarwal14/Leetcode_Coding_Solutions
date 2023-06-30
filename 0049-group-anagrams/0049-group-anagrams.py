2d0a2ecdd03297b9d69cba5ed7340138b192f1c2
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-07-01 00:57:01 +0530

    Time: 119 ms (56.09%), Space: 20.1 MB (60.45%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = collections.defaultdict(list)
+        for str in strs:
+            key = ''.join(sorted(str))
+            dict[key].append(str)
+
+        return dict.values()
