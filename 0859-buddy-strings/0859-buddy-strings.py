af7196706c33aa9f33be9819f9635eb19080eb8b
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-07-03 23:56:47 +0530

    Time: 66 ms (5.41%), Space: 16.7 MB (6.43%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,42 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        n=len(s)
+        m=len(goal)
+        if(n!=m):
+            return False
+        c=0
+        sc={}
+        gc={}
+        sl=[]
+        gl=[]
+        for i in range(n):
+            if(s[i]!=goal[i]):
+                c+=1
+                sl.append(s[i])
+                gl.append(goal[i])
+            if s[i] in sc:
+                sc[s[i]]+=1
+            else:
+                sc[s[i]]=1
+            if(goal[i] in gc):
+                gc[goal[i]]+=1
+            else:
+                gc[goal[i]]=1
+        sl.sort()
+        gl.sort()
+        if(c==2 and sl==gl):
+            return True
+        myKeys = list(sc.keys())
+        myKeys.sort()
+        stc = {i: sc[i] for i in myKeys}
+        myKeys = list(gc.keys())
+        myKeys.sort()
+        gtc = {i: gc[i] for i in myKeys}
+        f=0
+        for i in stc:
+            if(stc[i]>=2 and gtc[i]>=2):
+                f=1
+                break
+        if(c==0 and f==1):
+            return True
+        return False
