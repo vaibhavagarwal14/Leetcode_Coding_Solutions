1057068b9e76eb553581dcb75d9a5b16f4f60a39
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-07-19 23:57:29 +0530

    Time: 47 ms (69.13%), Space: 16.4 MB (28.78%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,6 +1,8 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        while(1):
-            if len(str(num))==1:
-                return num
-            num=sum([int(digit) for digit in str(num)])
+        # while(1):
+        #     if len(str(num))==1:
+        #         return num
+        #     num=sum([int(digit) for digit in str(num)])
+        
+        return 0 if num==0 else 1+(num-1)%9
