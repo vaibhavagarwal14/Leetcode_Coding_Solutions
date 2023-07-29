2e626b62c03d8d675ac2c34be781cd876eda99aa
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-07-29 17:08:35 +0530

    Time: 93 ms (5.06%), Space: 16.6 MB (80.98%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        store={}
+        for i in range(len(s)):
+            if t[i] in store.values() and s[i] not in store.keys():
+                return False
+            elif s[i] not in store:
+                store[s[i]]=t[i]
+            else:
+                if t[i]!=store[s[i]]:
+                    return False
+        return True
