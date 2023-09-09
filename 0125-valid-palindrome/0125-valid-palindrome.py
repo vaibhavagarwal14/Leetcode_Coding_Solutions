47bf4559ecdf8776a47da5d5bea5bb9f5d178bc7
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-09 19:08:06 +0530

    Time: 46 ms (83.08%), Space: 17.1 MB (63.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,13 +6,21 @@ class Solution:
             if i.isalnum():
                 st+=i
         n=len(st)
-        if(n%2==0):
-            s1=st[:n//2]
-            s2=st[n//2:]
-        else:
-            s1=st[:n//2]
-            s2=st[(n//2)+1:]
-        s2=s2[::-1]
-        if(s1==s2):
-            return True
-        return False
+        
+        # Approach 1:
+        # if(n%2==0):
+        #     s1=st[:n//2]
+        #     s2=st[n//2:]
+        # else:
+        #     s1=st[:n//2]
+        #     s2=st[(n//2)+1:]
+        # s2=s2[::-1]
+        # if(s1==s2):
+        #     return True
+        # return False
+        
+        # Approach 2:
+        for i in range(n//2):
+            if st[i]!=st[n-1-i]:
+                return False
+        return True
