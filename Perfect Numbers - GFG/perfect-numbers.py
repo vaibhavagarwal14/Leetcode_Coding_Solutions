d9e79703fbae58fa717688f407233fba92f8c6b7
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-12 20:40:07 +0530

    Added solution - LeetHub

---
+++ Perfect Numbers - GFG/perfect-numbers.py
@@ -0,0 +1,30 @@
+#User function Template for python3
+import math
+class Solution:
+    def isPerfectNumber(self, N):
+        # code here 
+        s=0
+        if N==1:
+            return 0
+        for i in range(1,int(math.sqrt(N))+1):
+            if N%i==0:
+                s+=i
+                if i!=N//i and i!=1:
+                    s+=N//i
+        if s==N:
+            return 1
+        return 0
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.isPerfectNumber(N))
+# } Driver Code Ends
