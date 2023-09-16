2b380d6cca3077776a33e756aedc9ea556461d8f
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-16 22:40:54 +0530

    Time: 139 ms (16.87%), Space: 23.1 MB (21.91%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,22 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n==0:
+            return [0]
+        if n==1:
+            return [0,1]
+        if n==2:
+            return [0,1,1]
+        if n==3:
+            return [0,1,1,2]
+        arr=[0]*(n+1)
+        j=2
+        arr[1]=1
+        arr[2]=1
+        arr[3]=2
+        for i in range(4,n+1):
+            if i==2**j:
+                arr[i]=1
+                j+=1
+            else:
+                arr[i]=arr[i-(2**(j-1))]+1
+        return arr
