f2bc43f2431e081828a4497bc1035ac76b54242d
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-16 22:46:55 +0530

    Time: 62 ms (94.78%), Space: 22.9 MB (93.79%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,22 +1,29 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        if n==0:
-            return [0]
-        if n==1:
-            return [0,1]
-        if n==2:
-            return [0,1,1]
-        if n==3:
-            return [0,1,1,2]
+        # Approach 1
+        # if n==0:
+        #     return [0]
+        # if n==1:
+        #     return [0,1]
+        # if n==2:
+        #     return [0,1,1]
+        # if n==3:
+        #     return [0,1,1,2]
+        # arr=[0]*(n+1)
+        # j=2
+        # arr[1]=1
+        # arr[2]=1
+        # arr[3]=2
+        # for i in range(4,n+1):
+        #     if i==2**j:
+        #         arr[i]=1
+        #         j+=1
+        #     else:
+        #         arr[i]=arr[i-(2**(j-1))]+1
+        # return arr
+        
+        # Approach 2 Bit Masking
         arr=[0]*(n+1)
-        j=2
-        arr[1]=1
-        arr[2]=1
-        arr[3]=2
-        for i in range(4,n+1):
-            if i==2**j:
-                arr[i]=1
-                j+=1
-            else:
-                arr[i]=arr[i-(2**(j-1))]+1
+        for i in range(1,n+1):
+            arr[i]=arr[i//2] + (i&1)
         return arr
