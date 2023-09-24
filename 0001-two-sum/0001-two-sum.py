d5889fcaf7b07c95cc00f3a530dde7e6e9bb97f4
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-24 11:26:46 +0530

    Time: 45 ms (99.90%), Space: 17.5 MB (47.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lst=[]
-        for i in range(len(nums)-1):
-            for j in range(i+1,len(nums)):
-                if(nums[i]+nums[j]==target):
-                    lst.append(i)
-                    lst.append(j)
-                    break
-        return lst
+        # Approach 1 - Brute Force
+        # lst=[]
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if(nums[i]+nums[j]==target):
+        #             lst.append(i)
+        #             lst.append(j)
+        #             break
+        # return lst
+        
+        #Approach 2 - Hashing
+        count={}
+        for i in range(len(nums)):
+            if target-nums[i] in count:
+                return [i,count[target-nums[i]]]
+            count[nums[i]]=i
+        
