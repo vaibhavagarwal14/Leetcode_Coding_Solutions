d32ec3de505e8eb9592a64b2cea3dc71055eec88
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-24 10:23:48 +0530

    Added solution - LeetHub

--- Find duplicates in an array - GFG/find-duplicates-in-an-array.py
+++ Find duplicates in an array - GFG/find-duplicates-in-an-array.py
@@ -1,16 +1,15 @@
 class Solution:
     def duplicates(self, arr, n): 
     	# code here
-    	lst=[]
     	for i in range(n):
     	    arr[arr[i]%n]+=n
+    	lst=[]
     	for i in range(n):
-    	    if arr[i] >= 2*n:
+    	    if arr[i]>=2*n:
     	        lst.append(i)
-    	if not lst:
+    	if len(lst)==0:
     	    return [-1]
     	return lst
-    	
     	    
 
 
