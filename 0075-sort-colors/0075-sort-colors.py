4d8ee0d42e558a752ea45223791241d2ed520584
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-24 12:23:06 +0530

    Time: 37 ms (80.77%), Space: 16.1 MB (89.20%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,18 +3,36 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        c=[0,0,0]
-        for i in nums:
-            if i == 0:
-                c[0]+=1
-            elif i == 1:
-                c[1]+=1
+        # Approach 1 - Sort
+        
+        # Approach 2 - use counters
+        # c=[0,0,0]
+        # for i in nums:
+        #     if i == 0:
+        #         c[0]+=1
+        #     elif i == 1:
+        #         c[1]+=1
+        #     else:
+        #         c[2]+=1
+        # for i in range(c[0]):
+        #     nums[i]=0
+        # for i in range(c[1]):
+        #     nums[i+c[0]]=1
+        # for i in range(c[2]):
+        #     nums[i+c[0]+c[1]]=2
+        
+        # Approach 3 - DNF
+        low=0
+        mid=0
+        h=len(nums)-1
+        while(mid<=h):
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            elif nums[mid]==1:
+                mid+=1
             else:
-                c[2]+=1
-        for i in range(c[0]):
-            nums[i]=0
-        for i in range(c[1]):
-            nums[i+c[0]]=1
-        for i in range(c[2]):
-            nums[i+c[0]+c[1]]=2
+                nums[mid],nums[h]=nums[h],nums[mid]
+                h-=1
         
