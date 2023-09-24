16380d740bc5aa1f83425ce4927439b0443fe0a4
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-24 20:04:58 +0530

    Time: 43 ms (38.63%), Space: 16.3 MB (33.59%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,16 +1,26 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         # Approach 1 - BF - nCr - O(n*n*r)
+        
         # Approach 2 - Generating individual rows using nCr(better) - O(n^2)
-        def generateRow(row):
-            temp=[1]
-            ans=1
-            for col in range(1,row):
-                ans=ans*(row-col)
-                ans=ans//col
-                temp.append(ans)
-            return temp
+        # def generateRow(row):
+        #     temp=[1]
+        #     ans=1
+        #     for col in range(1,row):
+        #         ans=ans*(row-col)
+        #         ans=ans//col
+        #         temp.append(ans)
+        #     return temp
+        # ans=[]
+        # for i in range(numRows):
+        #     ans.append(generateRow(i+1))
+        # return ans
+        
+        # Approach 3 - Using addition as seen in the figure
         ans=[]
         for i in range(numRows):
-            ans.append(generateRow(i+1))
+            ans.append([1]*(i+1))
+        for i in range(2,numRows):
+            for j in range(1,len(ans[i])-1):
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j]
         return ans
