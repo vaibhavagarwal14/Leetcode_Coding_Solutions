268f514df1454f38fd83ffd189afeaa221c3caa2
Vaibhav Agarwal <98638575+vaibhavagarwal14@users.noreply.github.com>
2023-09-24 12:35:38 +0530

    Time: 155 ms (39.60%), Space: 17.8 MB (90.60%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        # Approach 1 - Hashing
         # count={}
         # for i in nums:
         #     if i not in count:
@@ -12,10 +13,14 @@ class Solution:
         #         if count[key]==mx:
         #             return key
         
+        # Approach 2 - Moore's Voting Algorithm
         ans=0
         count=0
         for i in nums:
             if count==0:
                 ans=i
-            count+=(1 if ans==i else -1)
+            if ans==i:
+                count+=1
+            else:
+                count-=1
         return ans
